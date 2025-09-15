f5f9fdb59576c4ea0c662a3de4ac2127ad732bee
Albenis Ruiz <albenis1818@gmail.com>
2025-09-14 21:49:51 -0400

    update

--- src/components/Tool.jsx
+++ src/components/Tool.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { ButtonEnter } from './ButtonEnter.jsx';
 
 export function Tool({ drugs, isVisibleEnter, isVisible, onClickEnter,  onClickHandleClick }) {
@@ -6,15 +6,9 @@ export function Tool({ drugs, isVisibleEnter, isVisible, onClickEnter,  onClickH
 
     function handleChangeInInput(e) {
         setWordSearch(e.target.value.toLowerCase());
-    }
-    
-    useEffect(() => {
-    // Llama a isVisibleEnter solo si wordSearch no está vacío
-    if (wordSearch.trim() !== '') {
         isVisibleEnter();
     }
-    }, [wordSearch, isVisibleEnter]);
-
+    
     return(
         <>
             <form className='flex relative w-11/12 justify-center'>
